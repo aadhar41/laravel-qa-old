07d55be243ec80b0fe2e18078b408763a6375c38
aadhar gaur <aadhar41@gmail.com>
2020-05-21 21:47:05 +0530

    Generate question model via artisan model generator

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -36,4 +37,17 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function questions()
+    {
+        return $this->hasMany(Question::class);
+    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        // $this->attributes['slug'] = str_slug($value);
+        $this->attributes['slug'] = Str::slug($value, '-');
+    }
+
 }
